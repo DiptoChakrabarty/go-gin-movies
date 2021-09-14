eac1ce25e93a0a7874062876597119b2d7003cb1
DiptoChakrabarty <diptochuck123@gmail.com>
2021-09-15 00:06:13 +0530

    basic setup

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/health",func(c *gin.Context){
+		c.JSON(200,gin.H{
+			"message": "Server Up and Running"
+		})
+	})
+
+	router.Run(":8000")
+}
