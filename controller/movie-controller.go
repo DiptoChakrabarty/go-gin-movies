e36bcbac3c47be028ffccab36f55ce2b7a1ae264
DiptoChakrabarty <diptochuck123@gmail.com>
2021-09-17 12:25:03 +0530

    controller

--- controller/movie-controller.go
+++ controller/movie-controller.go
@@ -2,10 +2,32 @@ package controller
 
 import (
 	"github.com/DiptoChakrabarty/go-gin-movies/operation"
+	"github.com/DiptoChakrabarty/go-gin-movies/service"
 	"github.com/gin-gonic/gin"
 )
 
 type MovieController interface {
 	GetAll() []operation.Movie
-	Save(c *gin.Context)
+	Save(c *gin.Context) operation.Movie
+}
+
+type controller struct {
+	svc service.MovieService
+}
+
+func New(svc service.MovieService) MovieController {
+	return controller{
+		svc: svc,
+	}
+}
+
+func (c *controller) GetAll() []operation.Movie {
+	return c.svc.GetAll()
+}
+
+func (c *controller) Save(ctx *gin.Context) operation.Movie {
+	var movie operation.Movie
+	ctx.BindJSON(&movie)
+	c.svc.Save(movie)
+	return movie
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/DiptoChakrabarty/go-gin-movies
 
 go 1.15
 
-require github.com/gin-gonic/gin v1.7.4
+require (
+	github.com/gin-gonic/gin v1.7.4
+	github.com/go-delve/delve v1.7.1
+)
